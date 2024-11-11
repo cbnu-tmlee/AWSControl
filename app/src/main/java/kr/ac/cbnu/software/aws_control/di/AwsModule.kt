ca08bd2bbe13cc00d1255223a78c32bf8ef77431
cbnu-tmlee <taekmin@cbnu.ac.kr>
2024-11-11 11:59:24 +0900

    feat: Implement the Hilt module for AWS EC2

---
+++ app/src/main/java/kr/ac/cbnu/software/aws_control/di/AwsModule.kt
@@ -0,0 +1,22 @@
+package kr.ac.cbnu.software.aws_control.di
+
+import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
+import aws.sdk.kotlin.services.ec2.Ec2Client
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object AwsModule {
+    @Provides
+    @Singleton
+    fun provideEc2Client(
+        staticCredentialsProvider: StaticCredentialsProvider
+    ) = Ec2Client {
+        region = "ap-northeast-2"
+        credentialsProvider = staticCredentialsProvider
+    }
+}
