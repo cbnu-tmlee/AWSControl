a6c1d768a6b1743c1e4e57369a79ea70ef3ff59e
cbnu-tmlee <taekmin@cbnu.ac.kr>
2024-11-15 00:32:43 +0900

    build: Add and update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.android.library) apply false
 }
 
 buildscript {
